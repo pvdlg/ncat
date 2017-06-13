da095efacc8749d704a0569d79c3f370e4d7ca24
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-06-13 02:54:58 -0400

    Tes CC

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,14 @@ module.exports = {
   'extends': [
     '@metahub/eslint-config/node-config',
     '@metahub/eslint-config/es6-config'
-  ]
+  ],
+  'parserOptions': {
+    'ecmaVersion': 8,
+    'sourceType': 'module',
+    'ecmaFeatures': {
+      'globalReturn': true,
+      'impliedStrict': true,
+      'modules': true
+   }
+ }
 };
