e4343e6d2b0e8e64223593e294af5b27670176aa
Pierre Vanduynslager <pierre.denis.vanduynslager@gmail.com>
2017-06-13 02:58:03 -0400

    other test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,15 @@ module.exports = {
   'extends': [
     '@metahub/eslint-config/node-config',
     '@metahub/eslint-config/es6-config'
-  ],
-  'parserOptions': {
-    'ecmaVersion': 8,
-    'sourceType': 'module',
-    'ecmaFeatures': {
-      'globalReturn': true,
-      'impliedStrict': true,
-      'modules': true
-   }
- }
+  ]
+ //  ,
+ //  'parserOptions': {
+ //    'ecmaVersion': 8,
+ //    'sourceType': 'module',
+ //    'ecmaFeatures': {
+ //      'globalReturn': true,
+ //      'impliedStrict': true,
+ //      'modules': true
+ //   }
+ // }
 };
